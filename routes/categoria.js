53569e06595ffb2f7ea286d5cd21d125fce8e0b6
NestorJavier <nestorjavier@alumnos.uaslp.edu.mx>
2020-03-19 19:38:09 -0600

    no cambios

--- routes/categoria.js
+++ routes/categoria.js
@@ -4,19 +4,14 @@ import categoriaController from '../controllers/CategoriaController';
 import auth from '../middlewares/auth' 
 
 const router = routerx();//Para poder usar como un objeto el modulo npm i express-promise-router --save
-//router.post('/add', auth.verifyAmacenero, categoriaController.add);//Esta ruta nos dara acceso al metodo add que establecimos y exportamos en controllers/CategoriaController
-router.post('/add', categoriaController.add);
+router.post('/add', auth.verifyAmacenero, categoriaController.add);//Esta ruta nos dara acceso al metodo add que establecimos y exportamos en controllers/CategoriaController
 //el vervo post es un metodo de petición http
 router.get('/query', auth.verifyAmacenero, categoriaController.query);
-//router.get('/list', auth.verifyAmacenero, categoriaController.list);
-router.get('/list', categoriaController.list);
-//router.put('/update', auth.verifyAmacenero, categoriaController.update);
-router.put('/update', categoriaController.update);
+router.get('/list', auth.verifyAmacenero, categoriaController.list);
+router.put('/update', auth.verifyAmacenero, categoriaController.update);
 router.delete('/remove', auth.verifyAmacenero, categoriaController.remove);
-//router.put('/activate', auth.verifyAmacenero, categoriaController.activate);
-router.put('/activate', categoriaController.activate);
-//router.put('/deactivate', auth.verifyAmacenero, categoriaController.deactivate);
-router.put('/deactivate', categoriaController.deactivate);
+router.put('/activate', auth.verifyAmacenero, categoriaController.activate);
+router.put('/deactivate', auth.verifyAmacenero, categoriaController.deactivate);
 
 export default router;
  
